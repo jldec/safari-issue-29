d6dfa5fa0c2d1d8cc739e54d7f31950f15b1b3e4
jldec <jurgen@jldec.me>
2025-07-31 20:02:05 +0100

    add normal link to NotFoundPage

--- src/worker.tsx
+++ src/worker.tsx
@@ -9,7 +9,11 @@ import { ClientComponentPage } from './app/pages/ClientComponentPage'
 export type AppContext = {}
 
 export default defineApp([
-  render(Document, [route('/', HomePage), route('/client-component', ClientComponentPage)]),
+  render(Document, [
+    route('/', HomePage),
+    route('/client-component', ClientComponentPage),
+    route('/not-found', NotFoundPage)
+  ]),
   route('/*', async () => {
     const stream = await renderToStream(
       <NotFoundPage />,
